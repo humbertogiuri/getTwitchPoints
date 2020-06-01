85f6d38b552a8dba0471b061640784fcd7c8ecb5
Humberto Giuri <humbertogiuri@gmail.com>
2020-06-01 14:53:08 -0300

    Extensao funcionando

---
+++ README.md
@@ -0,0 +1,3 @@
+Extensão feita para Google Chrome com intúito de recolher automaticamente toda vez que o Bônus aparecer para ser resgatado.
+
+A princípio está configurada apenas para as lives do cellinrj e lannik. Para adicionar novas lives no funcionamento, basta ir no arquivo manifest.json e adicionar o url da live no "matches".
--- content.js
+++ content.js
@@ -1,1 +1,13 @@
-console.log("Iniciando extensão");
+const interval = setInterval(() => {
+    const button = document.querySelector(".tw-button--success")
+
+    if(button) {
+        button.click()
+        console.log("Botão clicado e os cellinPoints foram coletados")
+    }
+
+    else {
+        console.log("Tu ainda não tem bônus para recolher!!")
+    }
+    
+}, 10000)
